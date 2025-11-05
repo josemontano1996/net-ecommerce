964a27a89e1d8af06e082f2081d189e5b43cf808
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2025-11-05 21:52:41 +0100

    created CRQS interfaces

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+            var product = new ProducesResponseTypeMetadata ct
+            // Save to database
+
+            // Return result
+            throw new NotImplementedException();
+        }
+    }
+}

5275d2d266c2b4fc133f2b64546e9edc03afd970
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2025-11-08 11:23:12 +0100

    feat: impmlemented get product by id

--- src/Services/Catalog/CatalogAPI/GlobalUsing.cs
+++ src/Services/Catalog/CatalogAPI/GlobalUsing.cs
@@ -1,5 +1,6 @@
 ﻿global using BuildingBlocks.CQRS;
 global using Carter;
+global using CatalogAPI.Exceptions;
 global using CatalogAPI.Models;
 global using Mapster;
 global using Marten;
--- src/Services/Catalog/CatalogAPI/Products/GetProductById/GetProductById.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProductById/GetProductById.cs
@@ -1,6 +1,45 @@
 ﻿namespace CatalogAPI.Products.GetProductById
 {
-  public class GetProductById
+  public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+  public record GetProductByIdResult(Product Product);
+
+  internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
   {
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+      logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+      var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+      if (product is null)
+      {
+        throw new ProductNotFoundException();
+      }
+
+      return new GetProductByIdResult(product);
+    }
+  }
+
+  // public record GetProductByIdRequest();
+  public record GetProductByIdResponse(Product Product);
+
+  public class GetProductByIdEndpoint : ICarterModule
+  {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+      app.MapGet("/product/{id}", async (Guid Id, ISender sender) =>
+      {
+        var result = await sender.Send(new GetProductByIdQuery(Id));
+
+        var response = result.Adapt<GetProductByIdResponse>();
+
+        return Results.Ok(response);
+      })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product by its Id")
+        .WithDescription("Get Product by Id");
+    }
   }
 }

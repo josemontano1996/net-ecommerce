7cfa1af43a68a7c7fbe53fad594003301795db56
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2025-11-14 18:00:28 +0100

    core: updated globals

--- src/Services/Catalog/CatalogAPI/GlobalUsing.cs
+++ src/Services/Catalog/CatalogAPI/GlobalUsing.cs
@@ -7,6 +7,8 @@ global using CatalogAPI.Data;
 global using CatalogAPI.Exceptions;
 global using CatalogAPI.Models;
 global using FluentValidation;
+global using HealthChecks.UI.Client;
 global using Mapster;
 global using Marten;
 global using MediatR;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
